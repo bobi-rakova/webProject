54f0c2d6b2e7cec33a310496511acb2494e457f9
Bobii <b.rakova@gmail.com>
2012-01-31 23:48:32 +0200

    daily :)

---
+++ GraboWebProject/Controllers/StatisticsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GraboWebProject.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        //
+        // GET: /Statistics/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ GraboWebProject/Controllers/UserProfileController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GraboWebProject.Controllers
+{
+    public class UserProfileController : Controller
+    {
+        //
+        // GET: /UserProfile/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
