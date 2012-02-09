849f0634ef32c7d526183c6e4a3352f3477baf81
unknown <stanev@stanev-PC.(none)>
2012-02-09 21:10:17 +0200

    eto

---
+++ GraboWebProject/Controllers/Helper.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+
+    public static class HtmlHelpers
+    {
+        public static MvcHtmlString ActionImage(this HtmlHelper html, string action, string routeV, string imagePath, string alt)
+        {
+            var url = new UrlHelper(html.ViewContext.RequestContext);
+
+            // build the <img> tag
+            var imgBuilder = new TagBuilder("img");
+            imgBuilder.MergeAttribute("src", url.Content(imagePath));
+            imgBuilder.MergeAttribute("alt", alt);
+            string imgHtml = imgBuilder.ToString(TagRenderMode.SelfClosing);
+
+            // build the <a> tag
+            var anchorBuilder = new TagBuilder("a");
+            anchorBuilder.MergeAttribute("href", url.Action(action, routeV));
+            anchorBuilder.InnerHtml = imgHtml; // include the <img> tag inside
+            string anchorHtml = anchorBuilder.ToString(TagRenderMode.Normal);
+
+            return MvcHtmlString.Create(anchorHtml);
+        }
+    }
--- GraboWebProject/Controllers/HomeController.cs
+++ GraboWebProject/Controllers/HomeController.cs
@@ -24,5 +24,7 @@ namespace GraboWebProject.Controllers
         {
             return View();
         }
+
     }
+    
 }
