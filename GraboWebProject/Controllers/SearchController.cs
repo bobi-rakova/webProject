4cb6e18ec76e031d45eff82f500ebb445cc9f92f
Tony Rizov <bajanak@dir.bg>
2012-02-11 16:04:03 +0200

    Intelligent commit

--- GraboWebProject/Controllers/SearchController.cs
+++ GraboWebProject/Controllers/SearchController.cs
@@ -11,9 +11,31 @@ namespace GraboWebProject.Controllers
         //
         // GET: /Search/
 
+        private GrabooDBEntities entities = new GrabooDBEntities();
+        private List<Product> foundProducts = null;
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Find(FormCollection values)
+        {
+            string criteria = values["criteria"];
+
+            foundProducts = entities.Products.Where(x => x.Name.Contains(criteria)).ToList();
+
+            return View(foundProducts);
+        }
+
+        public ActionResult SelectProduct(FormCollection collection)
+        {
+            string selected = (string)Request["selected"];
+
+            var foundProduct = entities.Products.Where(x => x.Name == selected).ToList().ElementAt(0);
+
+            return View(foundProduct);
         }
 
     }
